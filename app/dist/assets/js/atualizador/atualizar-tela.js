ad92711bc86bd68999ebba2f1857e73a4e4d843b
Carlos Eduardo <105376328+delfo2@users.noreply.github.com>
2023-03-02 10:29:33 -0300

    quase definitivo

--- app/dist/assets/js/atualizador/atualizar-tela.js
+++
@@ -1,6 +0,0 @@
-import { Limpador } from "./zera-tela.js";
-const limpador = new Limpador();
-export function atualizarTela(valor) {
-    limpador.limpaTela();
-    console.log('atualizando tela.');
-}
--- app/dist/assets/js/atualizador/zera-tela.js
+++
@@ -1,8 +0,0 @@
-export class Limpador {
-    constructor(tela = document.querySelector('[data-tela=""]')) {
-        this.tela = tela;
-    }
-    limpaTela() {
-        this.tela.value = '0';
-    }
-}
